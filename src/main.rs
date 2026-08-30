6457bc8c873f1185bd5baa506970db2c210b7229
Rahul Kulkarni <kdrahul@github.com>
2026-08-30 22:24:04 +0530

    Added more code

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,23 @@ use serde::Deserialize;
 use std::{io::Read, net::TcpStream};
 
 // Server receives this
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 enum ClientFrame {
-    Join(Join),
+    Join { timestamp: String, user: User },
     Say { body: Body },
     Ping,
     Leave,
+    Error,
 }
-struct Message {
-    nick: String,
-    body: Body,
-}
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct User {
+    id: String,
     name: String,
     nick: String,
-    id: String,
 }
 
-#[derive(Deserialize)]
-struct Join {
-    timestamp: String,
-    user: User,
-}
-
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Body {}
 
 fn main() -> std::io::Result<()> {
@@ -42,6 +34,18 @@ fn main() -> std::io::Result<()> {
 fn handle_client(stream: Result<TcpStream, std::io::Error>) -> Result<(), std::io::Error> {
     let mut buffer = String::new();
     stream?.read_to_string(&mut buffer)?;
-    println!("{buffer}");
+    let value: ClientFrame = serde_json::from_str(buffer.as_str()).unwrap_or_else(|e| {
+        eprintln!("Error reading data from stream: {:?}", e);
+        ClientFrame::Error
+    });
+    match value {
+        ClientFrame::Join { timestamp, user } => {}
+        ClientFrame::Say { body } => {}
+        ClientFrame::Ping => todo!(),
+        ClientFrame::Leave => todo!(),
+        ClientFrame::Error => todo!(),
+    }
     Ok(())
 }
+
+fn handle_message(body: Body) {}
