87e0b344793c17f05e88e5a0392931e89a252e53
Rahul Kulkarni <kdrahul@github.com>
2026-08-30 15:59:45 +0530

    Updated code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
+use serde::Deserialize;
 use std::{io::Read, net::TcpStream};
+
+// Server receives this
+#[derive(Deserialize)]
+enum ClientFrame {
+    Join(Join),
+    Say { body: Body },
+    Ping,
+    Leave,
+}
+struct Message {
+    nick: String,
+    body: Body,
+}
+#[derive(Deserialize)]
+struct User {
+    name: String,
+    nick: String,
+    id: String,
+}
+
+#[derive(Deserialize)]
+struct Join {
+    timestamp: String,
+    user: User,
+}
+
+#[derive(Deserialize)]
+struct Body {}
 
 fn main() -> std::io::Result<()> {
     let listener = std::net::TcpListener::bind("127.0.0.1:9521")?;
